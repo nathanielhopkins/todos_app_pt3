312a9372470075740814a9f30834b556daa48d35
“NathanielHopkins” <nathanielchopkins@gmail.com>
2021-09-08 16:13:11 -0700

    create StaticPagesController with method root; create static_pages/root view that renders a div with id root and content "React is not working"; modify routes.rb to root to static_pages/root

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root to: 'static_pages#root'
   namespace :api, defaults: { format: :json } do 
     resources :todos, only: [:index, :show, :create, :update, :destroy]
   end
