c756225876d3b1c55b8e85d59dbab646c7db93f0
“NathanielHopkins” <nathanielchopkins@gmail.com>
2021-09-08 17:20:47 -0700

    modify frontend/todo_redux.jsx to not use localStorage.state but to load a configureStore without arguements

--- frontend/todo_redux.jsx
+++ frontend/todo_redux.jsx
@@ -5,8 +5,7 @@ import Root from './components/root';
 
 
 document.addEventListener("DOMContentLoaded", () => {
-  const preloadedState = localStorage.state ? JSON.parse(localStorage.state) : {};
-  let store = configureStore(preloadedState);
-
-  ReactDOM.render(<Root store={store}/>, document.getElementById("root"));
+  const store = configureStore();
+  const root = document.getElementById('content');
+  ReactDOM.render(<Root store={store} />, root);
 });
