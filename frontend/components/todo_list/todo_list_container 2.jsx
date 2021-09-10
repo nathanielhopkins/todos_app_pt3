62e9bc7d56e6925e0b498a7442862af8686f6106
“NathanielHopkins” <nathanielchopkins@gmail.com>
2021-09-10 11:13:39 -0700

    add node_modules to git

---
+++ frontend/components/todo_list/todo_list_container 2.jsx
@@ -0,0 +1,21 @@
+import { connect } from "react-redux";
+import TodoList from '../todo_list/todo_list';
+import { allTodos } from "../../reducers/selectors";
+import { createTodo, receiveTodo, removeTodo, fetchTodos } from "../../actions/todo_actions";
+
+const mapStateToProps = state => ({
+  todos: allTodos(state),
+  errors: state.errors
+});
+
+const mapDispatchToProps = dispatch => ({
+  receiveTodo: todo => dispatch(receiveTodo(todo)),
+  removeTodo: todo => dispatch(removeTodo(todo)),
+  requestTodos: () => dispatch(fetchTodos()),
+  createTodo: todo => dispatch(createTodo(todo))
+});
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+  )(TodoList);
