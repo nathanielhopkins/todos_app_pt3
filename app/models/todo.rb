0205761981bd13d622de29f34170a7a1f991f95d
“NathanielHopkins” <nathanielchopkins@gmail.com>
2021-09-13 12:33:28 -0700

    write has_many :steps association in Todo model

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -1,4 +1,10 @@
 class Todo < ApplicationRecord
+  has_many(
+    :steps,
+    class_name: "Step",
+    foreign_key: :todoID,
+    primary_key: :id
+  )
   validates :title, :body, presence: true
   validates :done, inclusion: { in: [true, false] }
 end
