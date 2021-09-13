c009322b9865c944d6b52af47ac3989b9fe4f434
“NathanielHopkins” <nathanielchopkins@gmail.com>
2021-09-13 12:52:13 -0700

    add routes for steps in routes.rb; nest index and create inside todos, and add update and destroy in root level of api

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   root to: 'static_pages#root'
   namespace :api, defaults: { format: :json } do 
-    resources :todos, only: [:index, :show, :create, :update, :destroy]
+    resources :todos, only: [:index, :show, :create, :update, :destroy] do
+      resources :steps, only: [:index, :create]
+    end
+
+    resources :steps, only: [:update, :destroy]
   end
 end
