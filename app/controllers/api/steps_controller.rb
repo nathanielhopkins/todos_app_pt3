2735be1a6f7029c616f860913afab0051b26f5c7
“NathanielHopkins” <nathanielchopkins@gmail.com>
2021-09-13 13:35:50 -0700

    write out index, create, update, and destroy methods in Api::StepsController

--- app/controllers/api/steps_controller.rb
+++ app/controllers/api/steps_controller.rb
@@ -1,28 +1,40 @@
 class Api::StepsController < ApplicationController
   def index
-    
-  end
-  
-  def show
-    
+    @steps = Todo.find(params[:todo_id]).steps
+    render json: @steps
   end
 
   def create
+    @step = Step.new(step_params)
+    @step.todoId = params[:todo_id]
     
+    if @step.save 
+      render json: @step
+    else 
+      render json: @step.errors.full_messages, status: 422
+    end
   end
 
   def update
-    
+    @step = Step.find(params[:id])
+    if @step
+      @step.update(step_params)
+      render json: @step
+    else
+      render json: { message: 'not found', status: 404 }
+    end
   end
 
   def destroy
-    
+    @step = Step.find(params[:id])
+    @step.destroy
+    render json: @step
   end
 
   private
 
   def step_params
-    params.require(:step).permit(:title, :body, :done)
+    params.require(:step).permit(:title, :body, :done, :todoId)
   end
 
 end
