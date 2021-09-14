5f84ddcb955e16df18b72c1506957b9d86ce68a7
“NathanielHopkins” <nathanielchopkins@gmail.com>
2021-09-14 12:01:00 -0700

    fix typo in Step model associatoins and validations; it read todoID instead of todoId

--- app/models/step.rb
+++ app/models/step.rb
@@ -2,10 +2,10 @@ class Step < ApplicationRecord
   belongs_to(
     :todo, 
     class_name: 'Todo',
-    foreign_key: :todoID,
+    foreign_key: :todoId,
     primary_key: :id
   )
 
-  validates :title, :body, :todoID, presence: true
+  validates :title, :body, :todoId, presence: true
   validates :done, inclusion: { in: [true, false] }
 end
