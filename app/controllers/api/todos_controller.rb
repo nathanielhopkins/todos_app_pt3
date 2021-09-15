5e3c542e3e3a2cfccc7326faab445da8b79529a5
“NathanielHopkins” <nathanielchopkins@gmail.com>
2021-09-15 14:17:22 -0700

    add tag_name as passible array to todo_params in Api::TodosController; include: :tags in all successful renders in Api::TodosController

--- app/controllers/api/todos_controller.rb
+++ app/controllers/api/todos_controller.rb
@@ -1,17 +1,17 @@
 class Api::TodosController < ApplicationController
 
   def show
-    render json: Todo.find(params[:id])
+    render json: Todo.find(params[:id]), include: :tags
   end
 
   def index
-    render json: Todo.all
+    render json: Todo.all, include: :tags
   end
 
   def create
     @todo = Todo.new(todo_params)
     if @todo.save
-      render json: @todo
+      render json: @todo, include: :tags
     else
       render json: @todo.errors.full_messages, status: 422
     end
@@ -20,7 +20,7 @@ class Api::TodosController < ApplicationController
   def update
     @todo = Todo.find(params[:id])
     if @todo.update(todo_params)
-      render json: @todo
+      render json: @todo, include: :tags
     else 
       render json: @todo.errors.full_messages, status: 422
     end
@@ -29,11 +29,11 @@ class Api::TodosController < ApplicationController
   def destroy
     @todo = Todo.find(params[:id])
     @todo.destroy
-    render json: @todo
+    render json: @todo, include: :tags
   end
 
   private
   def todo_params
-    params.require(:todo).permit(:title, :body, :done)
+    params.require(:todo).permit(:title, :body, :done, tag_names: [])
   end
 end
