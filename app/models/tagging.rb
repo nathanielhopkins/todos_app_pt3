d32f7283dfb61dde59b362b98bd3620ea82c3099
“NathanielHopkins” <nathanielchopkins@gmail.com>
2021-09-15 14:06:13 -0700

    save belongs_to association in Tagging

--- app/models/tagging.rb
+++ app/models/tagging.rb
@@ -1,3 +1,17 @@
 class Tagging < ApplicationRecord
   validates :tag_id, :todo_id, presence: true
+
+  belongs_to(
+    :todo,
+    class_name: 'Todo',
+    foreign_key: :todo_id,
+    primary_key: :id
+  )
+
+  belongs_to(
+    :tag,
+    class_name: 'Tag',
+    foreign_key: :tag_id,
+    primary_key: :id
+  )
 end
