c5a698eeec9a87a67f783964a5224e34262f52bb
“NathanielHopkins” <nathanielchopkins@gmail.com>
2021-09-17 12:31:28 -0700

    write ensure_session_token in User model that assigns a SecureRandom::urlsafe_base64 to self.session_token; add after_initialize :ensure_session_token to User

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,9 @@ class User < ApplicationRecord
   attr_reader :password 
   validates :username, :password_digest, :session_token, presence: true
   validates :password, length: { minimum: 6, allow_nil: true }
+  after_initialize :ensure_session_token
+
+  def ensure_session_token
+    self.session_token = SecureRandom::urlsafe_base64
+  end
 end
