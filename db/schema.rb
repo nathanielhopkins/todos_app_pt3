901d01c28f1c1ce7d1c5ead088d1f36dd9805ef7
“NathanielHopkins” <nathanielchopkins@gmail.com>
2021-09-17 11:36:37 -0700

    create and run migration to create users table with :username, :password_digest, and :session_token

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_15_192849) do
+ActiveRecord::Schema.define(version: 2021_09_17_183303) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -43,6 +43,14 @@ ActiveRecord::Schema.define(version: 2021_09_15_192849) do
     t.string "title"
     t.string "body"
     t.boolean "done"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username", null: false
+    t.string "password_digest", null: false
+    t.string "session_token", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
