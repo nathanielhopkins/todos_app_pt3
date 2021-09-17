275ee48b09336ae9dd6f5cc8b3161e4f477c9c9e
“NathanielHopkins” <nathanielchopkins@gmail.com>
2021-09-17 15:17:10 -0700

    write create in SessionsController that tries User.find_by_credentials with passed username and password; if successful, it calls login! on user and redirects to root url; if not successful, a new User is initiated, a flash error is passed and :new is rendered

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,15 @@ class SessionsController < ApplicationController
   end
 
   def create
-    
+    @user = User.find_by_credentials(user_params)
+    if @user
+      login!(@user)
+      redirect_to root_url
+    else
+      @user = User.new 
+      flash.now[:errors] = { base: ['Invalid username or password'] }
+      render :new
+    end
   end
 
   def destroy
