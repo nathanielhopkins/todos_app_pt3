13723b98185e44b8b0f453a0835298d0f1a124a8
“NathanielHopkins” <nathanielchopkins@gmail.com>
2021-09-17 16:48:26 -0700

    add routes to routes.rb: add :new and :create routes for :users at root level; add :new, :create, and :destroy routes for session

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   root to: 'static_pages#root'
+
+  resources :users, only: [:new, :create]
+  resource :session, only: [:new, :create, :destroy]
+
   namespace :api, defaults: { format: :json } do 
     resources :todos, only: [:index, :show, :create, :update, :destroy] do
       resources :steps, only: [:index, :create]
