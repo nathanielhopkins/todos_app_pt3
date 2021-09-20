7255455b69ad34d4f5b6daff0648acbe2f84849a
“NathanielHopkins” <nathanielchopkins@gmail.com>
2021-09-20 13:43:38 -0700

    write has_many :todos association in User and belongs_to :user in Todo

--- app/models/todo.rb
+++ app/models/todo.rb
@@ -9,6 +9,8 @@ class Todo < ApplicationRecord
   has_many :taggings
   has_many :tags, through: :taggings, source: :tag
 
+  belongs_to :user
+
   validates :title, :body, presence: true
   validates :done, inclusion: { in: [true, false] }
 
